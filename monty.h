2b87fc5afcc8351c9e603472b2b14e48977a57ba
Tshegofato <basetsana_12@icloud.com>
2023-08-24 15:41:05 -0700

    uptodate

--- monty.h
+++ monty.h
@@ -1,5 +1,6 @@
 #ifndef MONTY_H
 #define MONTY_H
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -8,19 +9,19 @@
 #include <string.h>
 #include <ctype.h>
 /**
-* struct stack_s - doubly linked list representation of a stack (or queue)
+* struct stack_s - doubly linked list representation of stack (or queue)
 * @n: integer
-* @prev: points to the previous element of the stack (or queue)
-* @next: points to the next element of the stack (or queue)
+* @prev: points to the previous element of stack (or queue)
+* @next: points to the next element of stack (or queue)
 *
 * Description: doubly linked list node structure
 * for stack, queues, LIFO, FIFO Holberton project
 */
 typedef struct stack_s
 {
-​int n;
-​struct stack_s *prev;
-​struct stack_s *next;
+int n;
+struct stack_s *prev;
+struct stack_s *next;
 } stack_t;
 /**
 * struct bus_s - variables -args, file, line content
@@ -32,10 +33,10 @@ typedef struct stack_s
 */
 typedef struct bus_s
 {
-​char *arg;
-​FILE *file;
-​char *content;
-​int lifi;
+char *arg;
+FILE *file;
+char *content;
+int lifi;
 }  bus_t;
 extern bus_t bus;
 /**
@@ -48,8 +49,8 @@ extern bus_t bus;
 */
 typedef struct instruction_s
 {
-​char *opcode;
-​void (*f)(stack_t **stack, unsigned int line_number);
+char *opcode;
+void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
 ssize_t getstdin(char **lineptr, int file);
